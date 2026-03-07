a8cbc33f4e96a1f080a1dc5842edb5ce23f79f40
Nandeesh Hiremath <nandeesh_b_h@Nandeeshs-MacBook-Air.local>
2026-03-07 12:42:35 +0530

    updated

--- src/testing2.java
+++ src/testing2.java
@@ -1,15 +1,9 @@
+import java.util.*;
+class testing2 {
 
-class demoo{
-    static {
-        System.out.println("this is static area");
-    }
-    demoo(){
-        System.out.println("this is constructor");
-    }
-}
-public class testing2 {
-    static void main(String[] args) {
-        demoo d1=new demoo();
+    public static void main(String[] args) {
+
+
 
     }
 }
